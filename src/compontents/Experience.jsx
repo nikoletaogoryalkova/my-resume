548e4e523d9081407829ab4d75a7d300e9dc4fbd
Nikoleta Ogoryalkova <nikoletaogoryalkova@gmail.com>
2025-05-29 14:33:21 +0300

    add pink vector animate

--- src/compontents/Experience.jsx
+++ src/compontents/Experience.jsx
@@ -3,7 +3,7 @@ export default function Experience() {
         <section className="experience">
             <img src="" alt="vector-pink" />
             <div className="experience-text">
-            <h2>FrontEnd Developer</h2>
+            <h2>Front-End Developer</h2>
             <p>Worked closely with cross-functional teams, including designers, marketers, and backend developers,</p>
             <p>to bring projects from concept to completion. Championed agile methodologies, ensuring seamless</p>
             <p>workflows and faster project turnaround.</p>
--- src/compontents/Home.jsx
+++ src/compontents/Home.jsx
@@ -1,6 +1,16 @@
-
+import { useState, useEffect } from 'react';
 
 export default function Home() {
+    const [animate, setAnimate] = useState(false);
+
+    useEffect(() => {
+        // Optional delay (like setTimeout)
+        const timeout = setTimeout(() => {
+            setAnimate(true);
+        }, 300); // delay in milliseconds
+        return () => clearTimeout(timeout);
+    }, []);
+
     return (
         <>
             <section className="header">
@@ -11,16 +21,16 @@ export default function Home() {
                 <p>Contact</p>
             </section>
             <div className="home">
-                <img src="" alt="photo" />
                 <div className="home-text">
-                    <img src="" alt="vector-pink" />
+                    <img  className={`vector-pink ${animate ? 'animate-in' : ''}`} src="/VectorPink.png" alt="vector-pink" />
                     <h1>Nikoleta Ogoryalkova</h1>
-                    <p>Welcome to my online CV! I specialize in web
-                        development and have a passion for creating unique
-                        and innovative digital experiences.
-                        Let`s work together to bring your vision to life.</p>
+                    <p>Welcome to my online CV! I specialize in web</p>
+                    <p> development and have a passion for creating unique</p>
+                    <p>  and innovative digital experiences.</p>
+                    <p>  Let`s work together to bring your vision to life.</p>
                     <button className="read-more-button">Read More</button>
                 </div>
+                <img className="profile-photo" src="/NikoletaOgoryalkova.jpeg" alt="Profile-photo" />
             </div>
         </>
     );
