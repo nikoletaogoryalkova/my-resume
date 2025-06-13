d3aa7e55185651a9b83bd329bc284cfeeef48a67
Nikoleta Ogoryalkova <nikoletaogoryalkova@gmail.com>
2025-06-13 22:01:19 +0300

    add description for project2

--- src/components/ProjectDetails.jsx
+++ src/components/ProjectDetails.jsx
@@ -14,7 +14,7 @@ const projectInfo = {
     2: {
         title: 'PROJECT 02',
         images: ['/Project2.jpg', '/Project2-2.svg','/Project2-3.svg'],
-        description: 'Banners, Flyers, Graphic Design',
+        description: 'A collection of creative projects that bring ideas to life — from posters and menus for restaurants and events to website designs. Each piece is crafted to communicate a message, reflect a unique style, and connect with its intended audience.',
         links: [
             // { label: 'GitHub', url: 'https://github.com/project2' },
         ],
